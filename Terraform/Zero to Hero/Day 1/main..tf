50df3b55e0e3b6a5adc6ae32e4d78090cd1f08fc
Ranga Dananjaya <rangamaithreepala@gmail.com>
2024-07-09 07:50:29 +0530

    create t2.micro

--- Terraform/Zero to Hero/Day 1/main..tf
+++ Terraform/Zero to Hero/Day 1/main..tf
@@ -6,4 +6,8 @@ provider "aws" {
 resource "aws_instance" "this" {
   ami                     = "ami-04a81a99f5ec58529"
   instance_type           = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
 }
